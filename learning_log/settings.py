2dffee137b33995930d80ca8b911837389986935
Mohammad <mahmudreza27@gmail.com>
2021-04-05 20:51:06 +0600

    adding heroku in settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 
 
 LOGIN_URL = 'users:login'
+
+
+# Heroku settings
+django_heroku.settings(locals())
